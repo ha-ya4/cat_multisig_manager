92ba130a0b2c90a65726233d20e537e39f130a1e
ha-ya4 <nemgrkz-rustzun8@yahoo.co.jp>
2021-04-27 00:43:26 +0900

    起動に必要なものの準備と起動確認

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+const createWindow = (): void => {
+  const window = new BrowserWindow({
+    width: 1200,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      nodeIntegrationInWorker: false,
+      contextIsolation: true,
+    },
+  });
+
+  // 読み込む index.html。
+  // tsc でコンパイルするので、出力先の dist の相対パスで指定する。
+  window.loadFile('./index.html');
+
+  // 開発者ツールを起動する
+  window.webContents.openDevTools();
+};
+
+// Electronの起動準備が終わったら、ウィンドウを作成する。
+app.whenReady().then(createWindow);
+
+// すべての ウィンドウ が閉じたときの処理
+app.on('window-all-closed', () => {
+  // macOS 以外では、メインプロセスを停止する
+  // macOS では、ウインドウが閉じてもメインプロセスは停止せず
+  // ドックから再度ウインドウが表示されるようにする。
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // macOS では、ウインドウが閉じてもメインプロセスは停止せず
+  // ドックから再度ウインドウが表示されるようにする。
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
