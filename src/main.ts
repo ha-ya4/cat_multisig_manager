26d18fe84cbb52d7f95205a1c963a32682061da9
ha-ya4 <nemgrkz-rustzun8@yahoo.co.jp>
2021-05-01 00:21:53 +0900

    コード整形など微修正

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,22 @@
 import { app, BrowserWindow } from 'electron';
-import path from 'path';
 
 const createWindow = (): void => {
-  const window = new BrowserWindow({
-    width: 1200,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: false,
-      nodeIntegrationInWorker: false,
-      contextIsolation: true,
-    },
-  });
+    const window = new BrowserWindow({
+        width: 1200,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: false,
+            nodeIntegrationInWorker: false,
+            contextIsolation: true,
+        },
+    });
 
-  // 読み込む index.html。
-  // tsc でコンパイルするので、出力先の dist の相対パスで指定する。
-  window.loadFile('./index.html');
+    // 読み込む index.html。
+    // tsc でコンパイルするので、出力先の dist の相対パスで指定する。
+    window.loadFile('./index.html');
 
-  // 開発者ツールを起動する
-  window.webContents.openDevTools();
+    // 開発者ツールを起動する
+    window.webContents.openDevTools();
 };
 
 // Electronの起動準備が終わったら、ウィンドウを作成する。
@@ -25,18 +24,18 @@ app.whenReady().then(createWindow);
 
 // すべての ウィンドウ が閉じたときの処理
 app.on('window-all-closed', () => {
-  // macOS 以外では、メインプロセスを停止する
-  // macOS では、ウインドウが閉じてもメインプロセスは停止せず
-  // ドックから再度ウインドウが表示されるようにする。
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    // macOS 以外では、メインプロセスを停止する
+    // macOS では、ウインドウが閉じてもメインプロセスは停止せず
+    // ドックから再度ウインドウが表示されるようにする。
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  // macOS では、ウインドウが閉じてもメインプロセスは停止せず
-  // ドックから再度ウインドウが表示されるようにする。
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+    // macOS では、ウインドウが閉じてもメインプロセスは停止せず
+    // ドックから再度ウインドウが表示されるようにする。
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
 });
